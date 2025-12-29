bc311fc949c4fb4061e898fd718734dc27f48d8f
Sonya <sonenkkaa@gmail.com>
2025-12-30 00:19:32 +0300

    add user creation

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,11 @@
     
     <button @click="checkServer">Проверить сервер</button>
     <button @click="getAllUsers">Получить всех пользователей</button>
+
+    <input v-model="newUser.name" placeholder="Имя" />
+    <input v-model="newUser.email" placeholder="Email" type="email" />
+    <input v-model="newUser.age" placeholder="Возраст" type="number" />
+    <button @click="createUser">Создать</button>
     
     <h3>Ответ сервера:</h3>
     <pre>{{ response }}</pre>
@@ -12,8 +17,14 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const BASE_URL = 'http://localhost:4000'
+
+const newUser = ref({
+  name: '',
+  email: '',
+  age: ''
+})
 
 const response = ref('')
 const status = ref('')
@@ -26,8 +37,8 @@ const checkServer = async () => {
     response.value = data
     status.value = `Успех: статус ${res.status}`
   } catch (error) {
-    status.value = `Ошибка: ${error.message}`
-    response.value = error.toString()
+    status.value = `Ошибка: ${(error as Error).message}`
+    response.value = (error as Error).toString()
   }
 }
 
@@ -37,10 +48,35 @@ const getAllUsers = async () => {
     const res = await fetch(`${BASE_URL}/api/users`)
     const data = await res.json()
     response.value = JSON.stringify(data, null, 2)
-    status.value = `Успех! Статус: ${res.status}`
+    status.value = `Успех: статус: ${res.status}`
   } catch (error) {
-    status.value = `Ошибка: ${error.message}`
-    response.value = error.toString()
+    status.value = `Ошибка: ${(error as Error).message}`
+    response.value = (error as Error).toString()
+  }
+}
+
+const createUser = async () => {
+  try {
+    status.value = 'Создаем пользователя...'
+    const res = await fetch(`${BASE_URL}/api/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser.value)
+    })
+    const data = await res.json()
+    response.value = JSON.stringify(data, null, 2)
+    status.value = `Успех, статус: ${res.status}`
+    
+    newUser.value = {
+      name: '',
+      email: '',
+      age: ''
+    }
+  } catch (error) {
+    status.value = `Ошибка: ${(error as Error).message}`
+    response.value = (error as Error).toString()
   }
 }
 </script>
