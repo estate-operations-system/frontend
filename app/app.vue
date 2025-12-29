348b7fce862ea0ea04a8a0e67d16f41ffc9e2d02
Sonya <sonenkkaa@gmail.com>
2025-12-29 23:58:12 +0300

    add users list display

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,7 @@
     <h1>Тестирование API пользователей</h1>
     
     <button @click="checkServer">Проверить сервер</button>
+    <button @click="getAllUsers">Получить всех пользователей</button>
     
     <h3>Ответ сервера:</h3>
     <pre>{{ response }}</pre>
@@ -24,6 +25,19 @@ const checkServer = async () => {
     const data = await res.text()
     response.value = data
     status.value = `Успех: статус ${res.status}`
+  } catch (error) {
+    status.value = `Ошибка: ${error.message}`
+    response.value = error.toString()
+  }
+}
+
+const getAllUsers = async () => {
+  try {
+    status.value = 'Получаем пользователей...'
+    const res = await fetch(`${BASE_URL}/api/users`)
+    const data = await res.json()
+    response.value = JSON.stringify(data, null, 2)
+    status.value = `Успех! Статус: ${res.status}`
   } catch (error) {
     status.value = `Ошибка: ${error.message}`
     response.value = error.toString()
