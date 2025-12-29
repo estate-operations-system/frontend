c6cefbc5b091111656f5874a440bb64ad985985b
Sonya <sonenkkaa@gmail.com>
2025-12-29 23:47:39 +0300

    add server status request

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Тестирование API пользователей</h1>
+    
+    <button @click="checkServer">Проверить сервер</button>
+    
+    <h3>Ответ сервера:</h3>
+    <pre>{{ response }}</pre>
+    
+    <p>Статус: {{ status }}</p>
   </div>
 </template>
+
+<script setup>
+const BASE_URL = 'http://localhost:4000'
+
+const response = ref('')
+const status = ref('')
+
+const checkServer = async () => {
+  try {
+    status.value = 'Запрос отправляется...'
+    const res = await fetch(BASE_URL)
+    const data = await res.text()
+    response.value = data
+    status.value = `Успех: статус ${res.status}`
+  } catch (error) {
+    status.value = `Ошибка: ${error.message}`
+    response.value = error.toString()
+  }
+}
+</script>
