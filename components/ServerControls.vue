118e878b7b361ea29d7a5f524919034ee357bd04
Sonenka <87214826+Sonenka@users.noreply.github.com>
2026-01-03 16:51:53 +0300

    Add testing api
    * add server status request
    
    * add users list display
    
    * add user creation
    
    * add user operations by ID
    
    * create UserForm component
    
    * create UserOperations component
    
    * create ResponseDisplay component
    
    * create ServerControls component
    
    * create useUserApi composable
    
    * fix after review

---
+++ components/ServerControls.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="server-controls">
+    <button @click="$emit('check-server')">
+      Проверить сервер
+    </button>
+    <button @click="$emit('get-all-users')">
+      Получить всех пользователей
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+  'check-server': []
+  'get-all-users': []
+}>()
+</script>
