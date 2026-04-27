e0b02d08ffc70ab0e8270a23d1076d5fa75e3925
Sonenka <sonenkkaa@gmail.com>
2026-04-27 23:25:03 +0300

    rework app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,9 @@
 <template>
-  <div id="app" :class="{ 'app--home': isHomePage }">
+  <div id="app" class="app" :class="{ 'app__home': isHomePage }">
     <Header class="header"/>
 
-    <main class="main-content" :class="{ 'main-content--full-screen': isHomePage }">
-      <div v-if="!isHomePage" class="main-content__container">
-        <NuxtPage />
-      </div>
-      <template v-else>
-        <NuxtPage />
-      </template>
+    <main class="app__content">
+      <NuxtPage />
     </main>
   </div>
 </template>
@@ -32,56 +27,22 @@ onMounted(async () => {
 </script>
 
 <style lang="scss">
-html, body {
-  font-family: var(--eos-font-family);
-  color: var(--eos-color-primary-950);
-  background-color: var(--eos-color-primary-50);
-  height: 100%;
-}
-
-#app {
+.app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
   padding-top: var(--eos-space-m);
 
-  &.app--home {
+  &__home {
     background: url('/background.png');
     background-size: cover;
     background-position: center;
     background-attachment: fixed;
     background-repeat: no-repeat;
   }
-}
 
-.main-content {
-  flex: 1;
-  background-color: var(--eos-color-primary-50);
-  width: 100%;
-  padding: var(--eos-space-l);
-}
-
-.main-content--full-screen {
-  max-width: none;
-  margin: 0;
-  padding: 0;
-  background-color: transparent;
-  display: flex;
-  flex-direction: column;
-}
-
-.main-content__container {
-  max-width: 1600px;
-  margin: 0 auto;
-  height: 100%;
-}
-
-.header {
-  flex-shrink: 0;
-}
-
-#app.app--home :deep(.header__wrapper) {
-  margin: 0;
-  padding: var(--eos-space-m) var(--eos-space-l);
+  &__content {
+    padding-block: var(--eos-space-l);
+  }
 }
 </style>
